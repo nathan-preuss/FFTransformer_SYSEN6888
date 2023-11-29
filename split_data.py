8d220a7cb83776664b3d8beceff53aaa3dfd2e53
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2023-11-29 14:58:26 -0500

    set up new wind speed data, still needs processing

---
+++ split_data.py
@@ -0,0 +1,3 @@
+# script to split our big data file into 60% test, 20% val, and 20% train
+# following the original paper, we do the split based on time with no shuffling
+#TODO: eventually adjust model from hours ahead to year ahead forecasting
