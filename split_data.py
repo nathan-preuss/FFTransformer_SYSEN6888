a5f1eaa27981b1b13e52092984344bf5b9c3b0f5
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2023-11-29 15:51:31 -0500

    "fixed" column headers in data

--- split_data.py
+++ split_data.py
@@ -2,7 +2,7 @@
 # following the original paper, we do the split based on time with no shuffling
 #TODO: eventually adjust model from hours ahead to year ahead forecasting
 import pandas as pd
-df = pd.read_csv("dataset_example/WindData/dataset/Windspeeds_Full_TestTrainVal.csv")
+df = pd.read_csv("dataset_example/WindData/dataset/Windspeeds_Full_TestTrainVal.csv", header=None)
 print(df.shape[0])
 
 #get number of records in each split
@@ -10,11 +10,19 @@ train= int(0.6*df.shape[0])
 val = int(0.2*df.shape[0])
 
 #split df
-df_train = df.loc[:train,]
-df_val = df.loc[train:train+val,]
-df_test = df.loc[train+val:,]
+header = df.loc[:1,]
+df_train = df.loc[2:train,]
+df_val = df.loc[train+1:train+val,]
+df_test = df.loc[train+val+1:,]
+
+#rename header variables
+header.replace(['DATE', 'ATMP', 'PRES', 'DEWP', "GST", "WSPD"], ['time', 'air_temperature', 'air_pressure_at_sea_level', 'dew_point_temperature', "max(wind_speed_of_gust PT10M)", "wind_speed"], inplace=True)
+
+df_train = pd.concat([header,df_train])
+df_val = pd.concat([header,df_val])
+df_test = pd.concat([header,df_test])
 
 #write out csv
-df_train.to_csv("dataset_example/WindData/dataset/train/wind_data.csv")
-df_val.to_csv("dataset_example/WindData/dataset/val/wind_data.csv")
-df_test.to_csv("dataset_example/WindData/dataset/test/wind_data.csv")
+df_train.to_csv("dataset_example/WindData/dataset/train/wind_data.csv", index=False, header=False)
+df_val.to_csv("dataset_example/WindData/dataset/val/wind_data.csv", index=False, header=False)
+df_test.to_csv("dataset_example/WindData/dataset/test/wind_data.csv", index=False, header=False)
