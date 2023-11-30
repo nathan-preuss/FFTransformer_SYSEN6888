9120b10acd274b5c372621d93ff0b26d6c524d17
Nathan Preuss <nathan.b.preuss-1@ou.edu>
2023-11-30 09:10:41 -0500

    new dataset with 9 stations

--- split_data.py
+++ split_data.py
@@ -1,6 +1,6 @@
 # script to split our big data file into 60% test, 20% val, and 20% train
 # following the original paper, we do the split based on time with no shuffling
-#TODO: eventually adjust model from hours ahead to year ahead forecasting
+
 import pandas as pd
 df = pd.read_csv("dataset_example/WindData/dataset/Windspeeds_Full_TestTrainVal.csv", header=None)
 
@@ -18,13 +18,13 @@ df = df.transpose()
 
 #remove unnecessary headings
 df.columns = [i for i in range(df.shape[1])] #TODO check new number of columns
-df = df.drop(columns=[i for i in range(13,24)], axis = 1)
+df = df.drop(columns=[i for i in range(9,18)], axis = 1)
 
 # reorder appropriate headings
-tide = [i for i in range(0,13)]
-time = [i for i in range(94, 107)]
-first = [i for i in range(24, 93)]
-last = [i for i in range(107, 116)]
+tide = [i for i in range(0, 9)]
+time = [i for i in range(72, 81)]
+first = [i for i in range(18, 72)]
+last = [i for i in range(81, 90)]
 time.extend(first)
 time.extend(tide)
 time.extend(last)
